4fab0c2c4cd9516ae4b865aaf95c988f1282288f
Sergiy Egoshyn <egoshin.sergey@gmail.com>
2026-07-30 13:09:36 +0300

    Native adaptation for Win on Arm  #15

--- make.bat
+++ make.bat
@@ -6,10 +6,13 @@ SET msbuild="%%F"
 ECHO %msbuild%
 
 @%msbuild% winUpdateMiniTool.sln /t:restore /p:RestorePackagesConfig=true
-@%msbuild% winUpdateMiniTool.sln /t:Rebuild /p:DebugType=None /p:Configuration=Release
+@%msbuild% winUpdateMiniTool.sln /t:Rebuild /p:DebugType=None /p:Configuration=Release /p:TargetFramework=net472 /p:RuntimeIdentifier=win-x64
+md winUpdateMiniTool\bin\publish\
+copy /Y winUpdateMiniTool\bin\net472\win-x64\* winUpdateMiniTool\bin\publish\
 
 rem @%msbuild% winUpdateMiniTool.sln /t:restore /p:RestorePackagesConfig=true /p:TargetFramework=net8.0-windows /p:RuntimeIdentifier=win-x64
-rem @%msbuild% winUpdateMiniTool.sln /t:Rebuild /p:DebugType=None /p:Configuration=Release /p:TargetFramework=net8.0-windows /p:RuntimeIdentifier=win-x64
+@%msbuild% winUpdateMiniTool/winUpdateMiniTool.csproj /t:Publish /p:DebugType=None /p:Configuration=Release /p:TargetFramework=net8.0-windows /p:RuntimeIdentifier=win-x64 /p:SelfContained=false /p:PublishSingleFile=true /p:PublishDir=bin\publish\ /p:DeleteExistingFiles=false
+@%msbuild% winUpdateMiniTool/winUpdateMiniTool.csproj /t:Publish /p:DebugType=None /p:Configuration=Release /p:TargetFramework=net8.0-windows /p:RuntimeIdentifier=win-arm64 /p:SelfContained=false /p:PublishSingleFile=true /p:AssemblyName=winUpdateMiniTool.arm64 /p:PublishDir=bin\publish\ /p:DeleteExistingFiles=false
 
 if errorlevel 1 goto error
 
