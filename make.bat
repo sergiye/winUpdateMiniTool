46809fbd916a856f3f0285e2d3c1c5e1503643be
Sergiy Egoshyn <egoshin.sergey@gmail.com>
2026-07-31 22:04:25 +0300

    Native adaptation for Win on Arm  #15

--- make.bat
+++ make.bat
@@ -11,7 +11,7 @@ md winUpdateMiniTool\bin\publish\
 copy /Y winUpdateMiniTool\bin\net472\win-x64\* winUpdateMiniTool\bin\publish\
 
 rem @%msbuild% winUpdateMiniTool.sln /t:restore /p:RestorePackagesConfig=true /p:TargetFramework=net8.0-windows /p:RuntimeIdentifier=win-x64
-@%msbuild% winUpdateMiniTool/winUpdateMiniTool.csproj /t:Publish /p:DebugType=None /p:Configuration=Release /p:TargetFramework=net8.0-windows /p:RuntimeIdentifier=win-x64 /p:SelfContained=false /p:PublishSingleFile=true /p:PublishDir=bin\publish\ /p:DeleteExistingFiles=false
+@%msbuild% winUpdateMiniTool/winUpdateMiniTool.csproj /t:Publish /p:DebugType=None /p:Configuration=Release /p:TargetFramework=net8.0-windows /p:RuntimeIdentifier=win-x64 /p:SelfContained=false /p:PublishSingleFile=true /p:AssemblyName=winUpdateMiniTool.core /p:PublishDir=bin\publish\ /p:DeleteExistingFiles=false
 @%msbuild% winUpdateMiniTool/winUpdateMiniTool.csproj /t:Publish /p:DebugType=None /p:Configuration=Release /p:TargetFramework=net8.0-windows /p:RuntimeIdentifier=win-arm64 /p:SelfContained=false /p:PublishSingleFile=true /p:AssemblyName=winUpdateMiniTool.arm64 /p:PublishDir=bin\publish\ /p:DeleteExistingFiles=false
 
 if errorlevel 1 goto error
